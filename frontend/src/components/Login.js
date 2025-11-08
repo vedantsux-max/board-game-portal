42603b23336080a8c02265a6fdfa717816dbadfe
ultburner00001 <ultburner.00001@gmail.com>
2025-11-08 20:57:20 +0530

    Login page edited

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -37,7 +37,7 @@ export default function Login({ setUser }) {
   return (
     <div className="login-container">
       <div className="login-box">
-        <h1>Welcome Back 👋</h1>
+        <h1> Login </h1>
         <form onSubmit={handleSubmit}>
           <div className="input-group">
             <label>Email</label>

ecf78560b31e11fdfebbb729211e0772d2fcef12
ultburner00001 <ultburner.00001@gmail.com>
2025-11-08 20:47:20 +0530

    Fix Login.css import and rebuild

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { loginUser } from "../api";
-import "./Login.css";
+import './Login.css';
 
 export default function Login({ setUser }) {
   const navigate = useNavigate();
